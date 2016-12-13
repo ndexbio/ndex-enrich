ee019e0f8b9a70241289f0d2224f200e2afdd1b9
Dexter Pratt <dexterpratt.bio@gmail.com>
2016-12-12 16:30:07 -0800

    similarity map tweaks

--- build_similarity_map.py
+++ build_similarity_map.py
@@ -3,6 +3,11 @@ __author__ = 'dexter'
 
 import argparse
 import similarity_map_utils as smu
+import ndex.beta.layouts as layouts
+import ndex.beta.toolbox as toolbox
+from os import getcwd
+import ndex.client as nc
+import ndex.networkn as networkn
 
 parser = argparse.ArgumentParser(description='build a similarity map from files in an e_set directory')
 
@@ -16,8 +21,22 @@ parser.add_argument('min_sub', action='store', type=float)
 
 arg = parser.parse_args()
 
-smu.create_similarity_map_from_enrichment_files(arg.map_name, arg.e_set_dir, arg.e_set_name, arg.min_sub)
+ndex = nc.Ndex("http://dev.ndexbio.org", "nci-test", "nci-test")
+response = ndex.get_network_as_cx_stream("87276dca-b8dd-11e6-a353-06832d634f41")
+template_cx = response.json()
+template_network = networkn.NdexGraph(template_cx)
 
+similarity_graph = smu.create_similarity_map_from_enrichment_files(arg.map_name, arg.e_set_dir, arg.e_set_name, arg.min_sub)
+
+
+toolbox.apply_network_as_template(similarity_graph, template_network)
+print "applied graphic style from " + str(template_network.get_name())
+
+layouts.apply_directed_flow_layout(similarity_graph, node_width=35, iterations=50, use_degree_edge_weights=True)
+
+filename = getcwd() + "/" + similarity_graph.get_name() + ".cx"
+print "writing map to: " + filename
+similarity_graph.write_to(filename)
 print "Map complete"
 
 
--- similarity_map_utils.py
+++ similarity_map_utils.py
@@ -1,42 +1,91 @@
 from __future__ import division
 from ndex.networkn import NdexGraph
 import data_model as dm
-from os import getcwd
+import operator
 
-def create_similarity_map_from_enrichment_files(map_name, directory, e_set_name, min_subsumption):
+# TODO: only one edge between each pair of nodes. Take the best one.
+
+
+def create_similarity_map_from_enrichment_files(map_name, directory, e_set_name, min_subsumption, max_edges=3):
     e_data = dm.EnrichmentData(directory)
     e_data.load_one_eset(e_set_name)
     e_set = e_data.get_e_set(e_set_name)
-    create_similarity_map(map_name, e_set, min_subsumption)
+    return create_similarity_map(map_name, e_set, min_subsumption, max_edges=max_edges)
 
-def create_similarity_map(name, e_set, min_subsumption, id_attribute="genes"):
-    similarity_map = NdexGraph()
+def create_similarity_map(name, e_set, min_subsumption, id_attribute="genes", max_edges=5):
+    similarity_graph = NdexGraph()
+    similarity_graph.set_name(name)
     set_name_to_node_id_map = {}
     id_sets = {}
-    for set_name in e_set.id_set_map:
-        id_set = e_set.id_set_map[set_name].set
-        id_sets[set_name] = id_set
+    for network_id in e_set.id_set_map:
+        id_set_object = e_set.id_set_map[network_id]
+        network_name = id_set_object.name
+        id_set = id_set_object.set
+        id_sets[network_id] = id_set
         att = {id_attribute: list(id_set)}
-        node_id = similarity_map.add_new_node(set_name, att)
-        set_name_to_node_id_map[set_name] = node_id
-        
-    for set_name_1 in id_sets.keys():
-        source_node_id = set_name_to_node_id_map[set_name_1]
-        for set_name_2 in id_sets.keys():
-            if set_name_1 != set_name_2:
-                overlap = list(id_sets[set_name_1].intersection(id_sets[set_name_2]))
+        node_id = similarity_graph.add_new_node(network_name, att)
+        set_name_to_node_id_map[network_id] = node_id
+    source_similarities = {}
+    for network_id_1 in id_sets.keys():
+        source_node_id = set_name_to_node_id_map[network_id_1]
+
+        list_1 = list(id_sets[network_id_1])
+        set_1_size = len(list_1)
+        similarities = []
+        for network_id_2 in id_sets.keys():
+
+            if network_id_1 != network_id_2:
+                set_1 = id_sets[network_id_1]
+                set_2 = id_sets[network_id_2]
+                target_node_id = set_name_to_node_id_map[network_id_2]
+                list_2 = list(id_sets[network_id_2])
+                set_2_size = len(list_2)
+                overlap = list(set_1.intersection(set_2))
                 size_overlap=len(overlap)
                 if size_overlap != 0:
-                    set_2_size = len(list(id_sets[set_name_2]))
-                    subsumes_measure=size_overlap/set_2_size
-                    if subsumes_measure > min_subsumption:
-                        target_node_id = set_name_to_node_id_map[set_name_2]
-                        atts = {"sub" : subsumes_measure, "overlap": overlap, "overlap_size": size_overlap}
-                        similarity_map.add_edge_between(source_node_id, target_node_id, "subsumed_by", atts)
 
-    filename = getcwd() + "/" + name + ".cx"
-    print "writing map to: " + filename
-    similarity_map.write_to(filename)
+                    subsumes = size_overlap/set_2_size
+                    #subsumes_1 = size_overlap/set_2_size
+                    #subsumes_2 = size_overlap/set_1_size
+                    #subsumes = min(subsumes_1, subsumes_2)
+                    if size_overlap > 3:
+                        print "overlap: %s %s" % (size_overlap, overlap)
+
+
+                    similarity = {"source_node_id": source_node_id,
+                                  "target_node_id": target_node_id,
+                                  "subsumes": subsumes}
+
+                    similarity["atts"] = {"subsumes": subsumes,
+                                          "overlap": overlap,
+                                          "overlap_size": size_overlap}
+                    similarities.append(similarity)
+                else:
+                    print "no overlap"
+
+        # rank the similarities
+        similarities = sorted(similarities, key=operator.itemgetter('subsumes'), reverse=True)
+        source_similarities[network_id_1] = similarities
+
+
+
+    # always include the most similar node to make sure that each node has at least one edge and the graph is connected
+    # don't connect more than max_edges
+    for network_id, similarities in source_similarities.iteritems():
+        count = 0
+        for similarity in similarities:
+            if count >= max_edges:
+                break
+            if count == 0 or similarity["subsumes"] > min_subsumption:
+                atts = similarity["atts"]
+                source_node_id = similarity["source_node_id"]
+                target_node_id = similarity["target_node_id"]
+
+                similarity_graph.add_edge_between(source_node_id, target_node_id, attr_dict=atts)
+            count = count + 1
+
+    return similarity_graph
+
 
 
 
